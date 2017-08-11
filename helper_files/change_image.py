1d03e1be7f76a5bb44fca127092d48a226097b9e
Sulai <seun.lasisi@yahoo.com>
2017-08-11 13:22:33 -0400

    Learning files

---
+++ helper_files/change_image.py
@@ -0,0 +1,38 @@
+import image_slicer
+import os
+from PIL.ImageOps import mirror 
+from PIL import Image
+import sys
+
+
+def get_basename(filename):
+    """Strip path and extension. Return basename."""
+    return os.path.splitext(os.path.basename(filename))[0]
+
+def split_images(idx, images_folder, cls, num_splits):
+  os.mkdir(os.path.join(images_folder, 'split_'+cls))
+  for directory, subdirectories, files in os.walk(os.path.join(images_folder, cls)):
+    for file in files:
+      file_name = os.path.join(images_folder, cls, file)
+      tiles = image_slicer.slice(file_name, num_splits, save = False)      
+      image_slicer.save_tiles(tiles, directory=os.path.join(images_folder, 'split_'+cls),prefix=get_basename(file_name))
+  print('Class', idx, 'set of images split.')
+
+
+
+
+'''
+This module flips an image horizontally so it can create 
+a slightly different duplicate to increase the sample size.
+'''
+def flip_images(cls, class_folder):
+  flip_suffix = '_mirror.jpg'
+  for directory, subdirectories, files in os.walk(class_folder):
+    for file in files:
+      file_name = os.path.join(class_folder, file)
+      im = Image.open(open(file_name, 'rb'))
+      mirror_image = mirror(im)
+      mirror_name = file.split('.')[0]+flip_suffix
+      os.chdir(class_folder)
+      mirror_image.save(mirror_name)
+  print('Class', cls, 'set of images flipped.')
